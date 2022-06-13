cdc5e5240a1ca53dfa4ecc54ba28103e79ddfe53
Lucian Miller <lucian.miller185@gmail.com>
2022-06-13 09:44:52 -0700

    Return instance when called

--- ToDoList.Solution/ToDoList/Models/Item.cs
+++ ToDoList.Solution/ToDoList/Models/Item.cs
@@ -9,6 +9,11 @@ namespace ToDoList.Models
     {
       Description = description;
       _instances.Add(this);
+
+      public static List<Item> GetAll()
+      {
+        return _instances;
+      }
     }
   }
 }
