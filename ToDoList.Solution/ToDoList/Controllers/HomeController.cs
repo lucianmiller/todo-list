134a59c9aeb4cf0011cd67d6fe8632471386f914
Lucian Miller <lucian.miller185@gmail.com>
2022-11-16 09:19:43 -0800

    Update Project

--- ToDoList.Solution/ToDoList/Controllers/HomeController.cs
+++ ToDoList.Solution/ToDoList/Controllers/HomeController.cs
@@ -1,15 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
-using ToDoList.Models;
 
 namespace ToDoList.Controllers
 {
   public class HomeController : Controller
   {
-    [Route("/")]
+    [HttpGet("/")]
     public ActionResult Index()
     {
-      Item starterItem = new Item("Add first item to To Do List");
-      return View(starterItem);
+      return View();
     }
 
   }
